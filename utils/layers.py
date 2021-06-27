a0ef5147e071c428982aa8876c1b014866e8ed48
LLL-Yoo <76723798+LLL-Yoo@users.noreply.github.com>
2021-06-27 15:07:19 +0800

    Update layers.py

--- utils/layers.py
+++ utils/layers.py
@@ -3,6 +3,36 @@ import tensorflow as tf
 
 conv1d = tf.layers.conv1d
 
+#为GAT核心模块
+'''
+3个比较核心的参数：
+seq:指的是输入的节点特征矩阵，大小为 [num_graph, num_node, fea_size]
+out_sz: 指的是变换后的节点特征维度，也就是Wh-后的节点表示维度
+bias_mat 是经过变换后的邻接矩阵，大小为 [num_node, num_node]
+
+
+作者首先将原始节点特征 seq 进行变换得到了 seq_fts。这里，作者使用卷积核大小为 1 的 1D 卷积模拟投影变换，
+投影变换后的维度为 out_sz。注意，这里投影矩阵W是所有节点共享，所以 1D 卷积中的多个卷积核也是共享的。
+seq_fts = tf.layers.conv1d(seq, out_sz, 1, use_bias=False)
+也就是说，seq_fts 的大小为 [num_graph, num_node, out_sz]。
+
+回顾前面的公式展开aT[hi||hj]=a1Thi+a2Thj也可以认为是投影变换，只不过投影到 1 维表示。注意，这里节点及其邻居的投影是分开的，有两套投影参数a1,a2 ，对应下面两个 conv1d 中的参数。
+f_1 = tf.layers.conv1d(seq_fts, 1, 1)
+f_2 = tf.layers.conv1d(seq_fts, 1, 1)
+
+
+经过 tf.layers.conv1d(seq_fts, 1, 1) 之后的 f_1 和 f_2 维度均为 [num_graph, num_node, 1]。
+
+将 f_2 转置之后与 f_1 叠加，通过广播得到的大小为 [num_graph, num_node, num_node] 的 logits，就是一个注意力矩阵：
+logitsij=(a->T[whi->||whj->])
+
+按照 GAT 的公式，我们只要对 logits 进行 softmax 归一化就可以拿到注意力权重 ，也就是代码里的 coefs。但是，这里为什么会多一项 bias_mat 呢？
+coefs = tf.nn.softmax(tf.nn.leaky_relu(logits) + bias_mat)
+因为的 logits 存储了任意两个节点之间的注意力值，但是，归一化只需要对每个节点的所有邻居的注意力进行
+所以，引入了 bias_mat 就是将 softmax 的归一化对象约束在每个节点的邻居上，如下式的红色部分。
+
+bias_mat 是如何实现的呢
+'''
 def attn_head(seq, out_sz, bias_mat, activation, in_drop=0.0, coef_drop=0.0, residual=False):
     with tf.name_scope('my_attn'):
         if in_drop != 0.0:
